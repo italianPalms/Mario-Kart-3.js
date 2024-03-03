716ce7b4aca539730af59500a738ce49734b8f9e
Eirik Nordsveen <eiriknordsveen@gmail.com>
2024-03-03 19:15:52 +0100

    remove the second box in the beginning

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -152,7 +152,6 @@ export const Experience = () => {
 
       <ParisBis position={[0, 0, 0]} />
       <ItemBox position={[-20, 2.5, -119]} />
-      <ItemBox position={[-10, 2.5, -119]} />
       <Coin position={[-30, 2, -119]} />
       <Skid />
       <Dust />
