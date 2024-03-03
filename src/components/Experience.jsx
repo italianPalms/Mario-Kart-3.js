b2ef64809446f1ddaa4f24aad1fedfa10e65fa59
Eirik Nordsveen <eiriknordsveen@gmail.com>
2024-03-03 19:14:34 +0100

    add one more box in the beginning

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -152,6 +152,7 @@ export const Experience = () => {
 
       <ParisBis position={[0, 0, 0]} />
       <ItemBox position={[-20, 2.5, -119]} />
+      <ItemBox position={[-10, 2.5, -119]} />
       <Coin position={[-30, 2, -119]} />
       <Skid />
       <Dust />
